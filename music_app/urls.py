89c34072443957df39db184fb263fd8441b199cf
tjburk <ty_burkhardt@verizon.net>
2022-12-23 10:58:58 -0500

    Added home page

--- music_app/urls.py
+++ music_app/urls.py
@@ -1,14 +1,14 @@
-"""internal_tempo URL Configuration
+"""music_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.music_app, name='music_app')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='music_app')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="internal_tempo/internal_tempo.html"), name="home"),
+    path('/', TemplateView.as_view(template_name="music_app/home.html"), name="music_app"),
+    path('internaltempo/', TemplateView.as_view(template_name="internal_tempo/internal_tempo.html"), name="internal_tempo"),
     path('accounts/', include('allauth.urls'), name="accounts"),
     path('admin/', admin.site.urls),
 ]
--- music_app/wsgi.py
+++ music_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for internal_tempo project.
+WSGI config for music_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internal_tempo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_app.settings')
 
 application = get_wsgi_application()
