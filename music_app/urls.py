337fa7439b696ec8b79f6088cb98650a6c47a551
tjburk <ty_burkhardt@verizon.net>
2022-12-24 00:28:26 -0500

    Added home page styling

--- music_app/urls.py
+++ music_app/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('/', TemplateView.as_view(template_name="music_app/home.html"), name="music_app"),
+    path('', TemplateView.as_view(template_name="music_app/home.html"), name="music_app"),
     path('internaltempo/', TemplateView.as_view(template_name="internal_tempo/internal_tempo.html"), name="internal_tempo"),
     path('accounts/', include('allauth.urls'), name="accounts"),
     path('admin/', admin.site.urls),
