f4b0faaee7f6c76256fb189f247091d5a16668bd
tjburk <ty_burkhardt@verizon.net>
2023-01-12 14:29:04 -0500

    Modified fields for exercise

--- technique_builder/forms.py
+++ technique_builder/forms.py
@@ -5,6 +5,10 @@ from .models import Exercise
 class ExerciseForm(forms.ModelForm):
     class Meta:
         model = Exercise
-        fields = ['name', 'sets', 'starting_bpm']
+        fields = ['name', 'description', 'sets', 'starting_bpm', 'dynamic']
         labels = {'starting_bpm': 'Bpm'}
-        help_texts = {}
+        help_texts = {'name': 'A name for the exercise',
+                      'description': 'A description for the exercise',
+                      'sets': 'How many repetitions of the exercise per completion',
+                      'starting_bpm': 'Initial bpm of the exercise',
+                      'dynamic': 'If selected, the bpm will slightly increase upon each successful completion of the exercise'}
--- technique_builder/models.py
+++ technique_builder/models.py
@@ -5,10 +5,12 @@ from django.conf import settings
 class Exercise(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500, blank=True)
     sets = models.IntegerField()
     starting_bpm = models.IntegerField()
     current_bpm = models.IntegerField()
-    mistakes = models.IntegerField(default=0)
+    completions = models.IntegerField(default=0)
+    dynamic = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
